b08593854bc21c633960fc5a6d583d1067a75ce2
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    gohan: Move Lineage specific overlays to overlay-lineage
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba18

--- device.mk
+++ device.mk
@@ -20,7 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/bq/gohan/gohan-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
